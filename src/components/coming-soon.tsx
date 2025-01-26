bc9bd388ba0fc2ea384347244e6da1d75d5ad8f3
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2025-01-26 19:44:56 +0530

    Create coming soon page

---
+++ src/components/coming-soon.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { section } from "framer-motion/client";
+import styles from "./coming-soon.module.css";
+import { motion } from "framer-motion";
+
+export default function ComingSoon() {
+  return (
+    <section className={styles.page}>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className={styles.motion}
+      >
+        <div className={styles.box}>COMING SOON</div>
+        <div className={styles.box2}>Stay Tuned</div>
+      </motion.div>
+    </section>
+  );
+}
